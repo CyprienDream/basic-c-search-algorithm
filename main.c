8a3a91fcafdabc7691247d97b397f8b098fb2b34
Cyprien Eckert <cipri.eckert@hotmail.fr>
2019-09-18 10:32:33 +0200

    Implement basic IO

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int main() {
     printf("Hello, World!\n");
+
+    int search_domain[20] = {1,2,4,5,6,7,55,6,10,99,11,12,13,14,15,16,17,18,19,30};
+    int search_value;
+
+    printf("Please input an integer\n");
+    scanf("%d", &search_value);
+
+
+    printf("%d", search_value);
+
     return 0;
 }
