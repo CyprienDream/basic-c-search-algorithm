5347de9df10a8cf3849e639d674a934e61cd8de1
Cyprien Eckert <cipri.eckert@hotmail.fr>
2019-09-18 10:35:26 +0200

    add basic search algorithm

--- main.c
+++ main.c
@@ -5,12 +5,17 @@ int main() {
 
     int search_domain[20] = {1,2,4,5,6,7,55,6,10,99,11,12,13,14,15,16,17,18,19,30};
     int search_value;
+    int i;
 
     printf("Please input an integer\n");
     scanf("%d", &search_value);
 
-
-    printf("%d", search_value);
+    for(i = 0; i < 20; i++) {
+        if(search_domain[i] == search_value) {
+            printf("Found value at: %d", i+1);
+            continue;
+        }
+    }
 
     return 0;
 }
